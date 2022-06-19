82c96445cd820bc6c46820a00b517d1b4ff1d8d5
Jan Ruettinger <service+github@janruettinger.com>
2022-06-19 16:05:08 -0700

    add rainbowkit package with demo connect button (including custom theme)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,114 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+    RainbowKitProvider,
+    getDefaultWallets,
+    Theme,
+    darkTheme,
+} from '@rainbow-me/rainbowkit';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider, webSocketProvider } = configureChains(
+    [
+        chain.mainnet,
+        chain.polygon,
+        chain.optimism,
+        chain.arbitrum,
+        ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
+            ? [chain.goerli, chain.kovan, chain.rinkeby, chain.ropsten]
+            : []),
+    ],
+    [
+        alchemyProvider({
+            // This is Alchemy's default API key.
+            // You can get your own at https://dashboard.alchemyapi.io
+            alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_ID,
+        }),
+        publicProvider(),
+    ]
+);
+
+const { connectors } = getDefaultWallets({
+    appName: 'My RainbowKit App',
+    chains,
+});
+
+const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+    webSocketProvider,
+});
+
+const DarkTheme = darkTheme();
+
+const myCustomTheme: Theme = {
+    ...DarkTheme,
+    colors: {
+        ...DarkTheme.colors,
+        // accentColor: '#000',
+    },
+    // colors: {
+    //     accentColor: '...',
+    //     accentColorForeground: '...',
+    //     actionButtonBorder: '...',
+    //     actionButtonBorderMobile: '...',
+    //     actionButtonSecondaryBackground: '...',
+    //     closeButton: '...',
+    //     closeButtonBackground: '...',
+    //     connectButtonBackground: '...',
+    //     connectButtonBackgroundError: '...',
+    //     connectButtonInnerBackground: '...',
+    //     connectButtonText: '...',
+    //     connectButtonTextError: '...',
+    //     connectionIndicator: '...',
+    //     error: '...',
+    //     generalBorder: '...',
+    //     generalBorderDim: '...',
+    //     menuItemBackground: '...',
+    //     modalBackdrop: '...',
+    //     modalBackground: '...',
+    //     modalBorder: '...',
+    //     modalText: '...',
+    //     modalTextDim: '...',
+    //     modalTextSecondary: '...',
+    //     profileAction: '...',
+    //     profileActionHover: '...',
+    //     profileForeground: '...',
+    //     selectedOptionBorder: '...',
+    //     standby: '...',
+    // },
+    // fonts: {
+    //     body: 'Inter',
+    // },
+    // radii: {
+    //     actionButton: '...',
+    //     connectButton: '...',
+    //     menuButton: '...',
+    //     modal: '...',
+    //     modalMobile: '...',
+    // },
+    // shadows: {
+    //     connectButton: '...',
+    //     dialog: '...',
+    //     profileDetailsAction: '...',
+    //     selectedOption: '...',
+    //     selectedWallet: '...',
+    //     walletLogo: '...',
+    // },
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <WagmiConfig client={wagmiClient}>
+            <RainbowKitProvider chains={chains} theme={myCustomTheme}>
+                <Component {...pageProps} />;
+            </RainbowKitProvider>
+        </WagmiConfig>
+    );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 const Home: NextPage = () => {
     return (
@@ -13,8 +14,13 @@ const Home: NextPage = () => {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <header className="p-4 bg-red-500">Header</header>
-            <main className="flex-grow bg-green-500">Content</main>
+            <header className="p-4 bg-red-500 flex flex-row justify-between">
+                <div>NAME </div>
+                <div>
+                    <ConnectButton />
+                </div>
+            </header>
+            <main className="flex-grow bg-green-500 p-4">Content</main>
             <footer className="p-4 bg-blue-500">Footer</footer>
         </div>
     );
